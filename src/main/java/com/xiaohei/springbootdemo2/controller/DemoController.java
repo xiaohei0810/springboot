7ece638d85ba35bf42a2ee1963cbb76677d9ed50
liangyusheng <dn960810>
2021-10-20 16:14:04 +0800

    springboot项目jenkins部署测试

---
+++ src/main/java/com/xiaohei/springbootdemo2/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.xiaohei.springbootdemo2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author liangyusheng@xiaomi.com
+ * @Date 2021/10/20 4:05 下午
+ * @Version 1.0
+ * @Describtion
+ */
+@RestController
+@RequestMapping("/xiaohei")
+public class DemoController {
+
+    @GetMapping("/get")
+    public String test(){
+        return "welcome to my home";
+    }
+
+}
