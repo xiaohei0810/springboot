6c0e67d336a678e2f95e017716c175fb50ef0bd4
liangyusheng <1127113014@qq.com>
2021-11-08 16:55:34 +0800

    "101010010101测试"

--- src/main/java/com/xiaohei/springbootdemo2/SpringBootDemo2Application.java
+++ src/main/java/com/xiaohei/springbootdemo2/SpringBootDemo2Application.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemo2Application {
 
     public static void main(String[] args) {
-        // 测试可以自动合并冲突的情况
+        // 测试可以自动合并冲突的情况10101010
         SpringApplication.run(SpringBootDemo2Application.class, args);
     }
 
