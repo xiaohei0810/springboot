e1ef60be37ba7d5614799aa361a15e5a4898b71c
liangyusheng <1127113014@qq.com>
2021-11-10 10:19:03 +0800

    "测试idea controller修改"

--- src/main/java/com/xiaohei/springbootdemo2/controller/DemoController.java
+++ src/main/java/com/xiaohei/springbootdemo2/controller/DemoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Describtion
  */
 @RestController
-@RequestMapping("/xiaohei")
+@RequestMapping("/xiaoheiidea")
 public class DemoController {
 
     @Autowired
